d67f43f05a37f3d5a644b504f0c3e098a193f42f
NikitaFilimonov <69960509+NikitaFilimonov@users.noreply.github.com>
2020-09-26 11:38:12 +0300

    HW2

--- src/ru/geekbrains/lessons/Main.java
+++ src/ru/geekbrains/lessons/Main.java
@@ -2,7 +2,80 @@ package ru.geekbrains.lessons;
 
 public class Main {
 
-    public static void main(String[] args) {
+        static class MyArraySizeException extends Exception {
+            MyArraySizeException(String msg) {
+                super(msg);
+            }
+        }
 
+        static class MyArrayDataException extends Exception {
+            MyArrayDataException(String msg) {
+                super(msg);
+            }
+        }
+
+        static class MyArraySize {
+            static int getArray(String[][] _myArray) throws MyArraySizeException,MyArrayDataException {
+                if(_myArray.length == 4 && _myArray[0].length == 4) {
+                    System.out.println("Array's length is correct");
+                } else {
+                    throw new MyArraySizeException("Array's length is not correct");
+                }
+
+                int sum = 0;
+                if(_myArray.length != 4) {
+                    throw new MyArraySizeException("Array's length is not correct");
+                }
+
+                for (int i=0;i<_myArray.length;i++) {
+                    if(_myArray[i].length != 4) {
+                        throw new MyArraySizeException("Array's length is not correct");
+                    }
+                    for(int j=0;j<_myArray[i].length;j++) {
+                        try {
+                            sum += Integer.parseInt(_myArray[i][j]);
+                        } catch (NumberFormatException e) {
+                            throw new MyArrayDataException("Unable to convert cell "+i+"x"+j);
+                        }
+                    }
+                }
+
+                Integer[][] intArray = new Integer[_myArray.length][_myArray[0].length];
+                int res=0;
+                for(int i=0;i<_myArray.length;i++) {
+                    for (int j=0;j<_myArray[0].length;j++) {
+                        intArray[i][j] = Integer.parseInt(_myArray[i][j]);
+                        i++;
+                        res += intArray[i][j];
+                    }
+                }
+                return sum;
+            }
+        }
+
+        static class MainArrayEx {
+            public static void main(String[] args) {
+                String[][] array = new String[4][4];
+                for(int i=0; i< array.length; i++){
+                    for(int j=0;j < array[i].length; j++){
+                        array[i][j]="Str"+j;
+                        System.out.print(array[i][j]+" ");
+                    }
+                    System.out.println();
+                }
+
+                try {
+                    try {
+                        int sum = MyArraySize.getArray(array);
+                    } catch (MyArraySizeException e) {
+                        e.printStackTrace();
+                        System.out.println(e.getMessage());
+                    }
+                } catch (MyArrayDataException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        }
     }
-}
+
